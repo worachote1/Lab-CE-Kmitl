40d10fed026f2a66d14bd61f36685e427d9b046f
worachote1 <68600257+worachote1@users.noreply.github.com>
2021-08-15 11:47:45 +0700

    commit my lab 6

--- Lab6/Lab6/Lab6.c
+++ Lab6/Lab6/Lab6.c
@@ -22,12 +22,22 @@ void gotoxy(int x, int y) {
 
 void draw_ship(int a, int b) {
 	gotoxy(a, b);
-	//Sleep(500);
+
 	setcolor(2, 4);
 	printf("<-0->");
 }
-void erase_ship(int x, int y) {
-	gotoxy(x, y);
+void draw_bullet(int a,int b) {
+	gotoxy(a,b);
+	setcolor(2, 4);
+	printf("^");
+}
+void erase_bullet(int a,int b) {
+	gotoxy(a, b);
+	setcolor(2, 0);
+	printf(" ");
+}
+void erase_ship(int a, int b) {
+	gotoxy(a, b);
 	setcolor(2,0);
 	printf("     ");
 }
@@ -40,37 +50,74 @@ int main() {
 	int x = 38, y = 20;
 	setcursor(0);
 	draw_ship(x, y);
+	bool LEFT = false;
+	bool RIGHT = false;
+	bool STOP = false;
+	int bullet[5] = { 0 };
+	int bx[5];
+	int by[5];
 
 	do {
 		if (_kbhit()) {
 			ch = _getch();
+			//check keyborad press
 			if (ch == 'a') {
-				if (x > 0) {
-					erase_ship(x, y);
-					draw_ship(--x, y);
-				}
-
+					LEFT = true;
+					RIGHT = false;
+					STOP = false;
 			}
 			if (ch == 'd') {
-				if (x < 80) {
-					erase_ship(x, y);
-					draw_ship(++x, y);				
-				}
-			}
-			if (ch == 'w') {
-				if (y > 0) {
-					erase_ship(x, y);
-					draw_ship(x, --y);
-				}
+				LEFT = false;
+				RIGHT = true;
+				STOP = false;
 			}
 			if (ch == 's') {
-				if (y < 40) {
-					erase_ship(x, y);	
-					draw_ship(x, ++y);
+				LEFT = false;
+				RIGHT = false;
+				STOP = true;				
+			}
+			//set bullet status if press k
+			if (ch == 'k') {
+				for (int i = 0; i < 5;i++) {
+					if (bullet[i]==0) {
+						bullet[i] = 1;
+						bx[i] = x + 3;
+						by[i] = y + 1;
+						draw_bullet(bx[i], by[i]);
+						break;
+					}
 				}
 			}
-
-			fflush(stdin);
+		}
+		fflush(stdin);
+		//check direction
+		//move left
+		if (LEFT == 1 && x > 0) {
+			erase_ship(x, y);
+			draw_ship(--x, y);
+		}
+		//move right
+		if (RIGHT == 1 && x < 80) {
+			erase_ship(x, y);
+			draw_ship(++x, y);
+		}
+		//stop moiving when press s
+		if (STOP == 1) {
+			draw_ship(x, y);
+		}
+		//shoot if bullet state is 1
+		for (int i = 0; i < 5;i++) {
+			if (bullet[i] != 0 && by[i]>0) {
+				erase_bullet(bx[i], by[i]);
+				by[i] -= 1;
+				draw_bullet(bx[i], by[i]);
+			}
+		}
+		//erase bullet if it hit the top
+		for (int i = 0; i < 5;i++) {
+			if (by[i]==0) {
+				erase_bullet(bx[i], by[i]);
+			}
 		}
 
 		Sleep(100);
